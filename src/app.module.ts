6b5577fbbf6a92a6b7d02c6a91b3f761f95d05a0
CrisGadea <cristianhernangadea@gmail.com>
2023-12-29 09:35:44 -0300

    basic structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { PurchasesModule } from './purchases/purchases.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProductsModule, PurchasesModule],
   controllers: [AppController],
   providers: [AppService],
 })
